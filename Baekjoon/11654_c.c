fe076b1c285a5b6a42bd4b412bea19afe18660d4
Uibeom <wjddmlqja17@gmail.com>
2021-08-05 23:02:24 +0900

    create .gitignore file & 11654_c.c
    .gitignore, ignore .vs/ folder
    11654_c, ASCII code problem

---
+++ Baekjoon/11654_c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int input = 0;
+    int output = 0;
+    // 예제 입력
+    scanf("%c", &input);
+    output = (int)input;
+
+    //예제 출력
+    printf("%d", output);
+
+    return 0;
+}
